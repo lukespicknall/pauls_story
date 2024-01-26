17ceac19ac1adc30ef3409872792a4878f2eff34
Luke Spicknall <lukejspicknall@gmail.com>
2024-01-25 20:57:39 -0500

    create images folder in src/ and put some images in to test. create images.js to handle importing images and exporting them in an array to index.js - which imports the images[] and loops through it to append the images to the page. this is my basic test for how to push images in to the environment.

---
+++ src/images.js
@@ -0,0 +1,14 @@
+import Pic1 from './images/dad1.jpg';
+import Pic2 from './images/dad2.jpg';
+
+const images = [];
+
+const dad1 = new Image();
+dad1.src = Pic1;
+images.push(dad1);
+
+const dad2 = new Image();
+dad2.src = Pic2;
+images.push(dad2);
+
+export default images;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './style.css';
+import images from './images';
 
 const content = document.getElementById('content');
+
+for (let i = 0; i < images.length; i += 1) {
+  const imageHolder = images[i];
+  content.appendChild(imageHolder);
+}
 
 const templatePage = document.createElement('div');
 templatePage.id = 'template-page';
